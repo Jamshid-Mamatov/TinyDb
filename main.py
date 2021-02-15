f6425680b645cd9f7240b15570763f01714f9de9
Jamshid <mamatovjamshid506@gmail.com>
2021-02-15 16:13:43 +0500

    truncate

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 from tinydb import TinyDB
 
 data_base=TinyDB("db.json")
+data_base.truncate()
 
 user1={"user_id":1,"username":"Jamshid"}
 user2={"user_id":2,"username":"Diyor"}
 user3={"user_id":3,"username":"Javohir"}
+user4={"user_id":4,"username":"Ali"}
 
-data_base.insert(user1)
-data_base.insert(user2)
-data_base.insert(user3)
 
-data_base.insert_multiple([user3,user1,user2])
+data_base.insert_multiple([user3,user1,user2,user4])
