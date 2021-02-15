5e8c7cbb9ffc5274afa54c547cbcad767ebd9865
Jamshid <mamatovjamshid506@gmail.com>
2021-02-15 15:10:27 +0500

    insert

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from tinydb import TinyDB
 
 data_base=TinyDB("db.json")
+
+user1={"user_id":1,"username":"Jamshid"}
+user2={"user_id":2,"username":"Diyor"}
+user3={"user_id":3,"username":"Javohir"}
+
+data_base.insert(user1)
+data_base.insert(user2)
+data_base.insert(user3)
