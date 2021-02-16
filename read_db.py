a758188872b8683590ca9a552036afd2ccfabb06
Jamshid <mamatovjamshid506@gmail.com>
2021-02-16 10:24:09 +0500

    remove

--- read_db.py
+++ read_db.py
@@ -2,8 +2,7 @@ from tinydb import TinyDB,Query
 
 data_base=TinyDB("db.json")
 q=Query()
+
+data_base.remove(q.user_id>2)
 data=data_base.all()
-user=data_base.search(q.user_id>2)
-
-print(user)
-
+print(data)
