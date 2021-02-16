1409c60d94692c8024daf96ad95c2a7b6aed6a09
Jamshid <mamatovjamshid506@gmail.com>
2021-02-16 10:19:11 +0500

    search

--- read_db.py
+++ read_db.py
@@ -1,6 +1,9 @@
-from tinydb import TinyDB
+from tinydb import TinyDB,Query
 
 data_base=TinyDB("db.json")
-
+q=Query()
 data=data_base.all()
-print(data)
+user=data_base.search(q.user_id>2)
+
+print(user)
+
